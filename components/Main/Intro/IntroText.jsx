73a59599e852fdb600442a1c7ec557a8d04cb333
israel1281 <kidoski1@gmail.com>
2022-10-27 05:35:22 -0700

    init commit

--- components/Main/Intro/IntroText.jsx
+++ components/Main/Intro/IntroText.jsx
@@ -1,38 +1,39 @@
-import React from "react";
+import React from 'react'
 
 const IntroText = () => {
   return (
-    <div className="mx-auto max-w-md px-4 sm:flex-col sm:items:center sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
-      <div className="lg:py-24 flex-col sm:flex sm:items-center">
-        <h1 className="mt-4 text-3xl tracking-tight font-medium text-white text-center sm:text-left md:text-center lg:text-left sm:mt-5 sm:text-4xl lg:mt-6 xl:text-5xl">
-          <span className="block">Save time by building</span>
-          <span className="text-whiteblock mt-4">
-            fast with Boldo Template
-          </span>
-        </h1>
-        <p className="mt-3 text-base text-center sm:text-left md:text-center lg:text-left text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-lg">
-          Funding handshake buyer business-to-business metrics iPad partnership.
-          First mover advantage innovator success deployment non-disclosure.
-        </p>
-        <div className="mt-10 sm:mt-12">
-          <div className="sm:max-w-xl sm:mx-auto lg:mx-0">
-            <div className="sm:flex md:mx-auto">
-              <div className="mt-3 sm:mt-0 sm:ml-3">
-                <button className="block text-lg w-full py-3 px-14 rounded-full shadow bg-[#69E6A6] text-[#0A2640] font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900">
-                  Buy template
-                </button>
-              </div>
-              <div className="mt-3 sm:mt-0 sm:ml-3">
-                <button className="block text-lg w-full py-3 px-12 rounded-full shadow border border-white text-white font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900">
-                  Explore
-                </button>
+    <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
+            <div className="lg:py-24">
+              <h1 className="mt-4 text-3xl tracking-tight font-medium text-white sm:mt-5 sm:text-4xl lg:mt-6 xl:text-5xl">
+                <span className="block text-center sm:text-left">Save time by building</span>
+                <span className="text-white text-center sm:text-left block mt-4">
+                  fast with Boldo Template
+                </span>
+              </h1>
+              <p className="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-lg">
+                Funding handshake buyer business-to-business metrics iPad
+                partnership. First mover advantage innovator success deployment
+                non-disclosure.
+              </p>
+              <div className="mt-10 sm:mt-12">
+                <div className="sm:max-w-xl sm:mx-auto lg:mx-0">
+                  <div className="sm:flex">
+                    <div className="mt-3 sm:mt-0 sm:ml-3">
+                      <button className="block text-lg w-full py-3 px-14 rounded-full shadow bg-[#69E6A6] text-[#0A2640] font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900">
+                        Buy template
+                      </button>
+                    </div>
+                    <div className="mt-3 sm:mt-0 sm:ml-3">
+                      <button className="block text-lg w-full py-3 px-12 rounded-full shadow border border-white text-white font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900">
+                        Explore
+                      </button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+  )
+}
 
-export default IntroText;
+export default IntroText
